24c039a81ed16e072a6b9310f57c552a4950575c
Lujan Ramirez <lujanramirez375@gmail.com>
2025-08-27 15:49:18 -0300

    03 stored function.sql

--- Scripts/03 stored function.sql
+++ Scripts/03 stored function.sql
@@ -1,68 +1,34 @@
    
---Contar cuántas reservas tiene un hotel--
-DELIMITER $$
-CREATE FUNCTION sf_ReservasPorHotel(p_idHotel TINYINT)
+--Devuelve la cantidad total de reservas realizadas en un hotel determinado.
+
+
+CREATE FUNCTION totalReservas(unIdHotel TINYINT)
 RETURNS INT
-DETERMINISTIC
+READS SQL DATA
 BEGIN
-   DECLARE total INT;
-   SELECT COUNT(*) INTO total
+   DECLARE resultado INT;
+   SELECT COUNT(*) INTO resultado
    FROM Reserva
-   WHERE idHotel = p_idHotel;
-   RETURN total;
-END $$
-
-
---Obtener nombre completo del cliente--
-DELIMITER $$
-CREATE FUNCTION sf_NombreCompletoCliente(p_dni INT)
-RETURNS VARCHAR(90)
-DETERMINISTIC
-BEGIN
-   DECLARE nombreCompleto VARCHAR(90);
-   SELECT CONCAT(nombre, ' ', apellido)
-   INTO nombreCompleto
-   FROM Cliente
-   WHERE dni = p_dni;
-   RETURN nombreCompleto;
-END $$
+   WHERE idHotel = unIdHotel;
+   RETURN resultado;
+END;
 
 
 
---Verificar si un hotel tiene más de 5 reservas--
-DELIMITER $$
-CREATE FUNCTION sf_HotelTieneMuchasReservas(p_idHotel TINYINT)
+
+--Devuelve 1 (TRUE) si hay algún transporte que sale en esa fecha, o 0 (FALSE) si no hay ninguno.
+
+
+CREATE FUNCTION hayTransporteDisponible(fecha DATETIME)
 RETURNS BOOLEAN
-DETERMINISTIC
+READS SQL DATA
 BEGIN
    DECLARE cantidad INT;
    SELECT COUNT(*) INTO cantidad
-   FROM Reserva
-   WHERE idHotel = p_idHotel;
-   RETURN cantidad > 5;
-END $$
+   FROM Trasporte
+   WHERE DATE(salida) = DATE(fecha);
+   RETURN cantidad > 0;
+END;
 
 
---Obtener cantidad total de los clientes--
-DELIMITER $$
-CREATE FUNCTION sf_TotalClientes()
-RETURNS INT
-DETERMINISTIC
-BEGIN
-   DECLARE total INT;
-   SELECT COUNT(*) INTO total FROM Cliente;
-   RETURN total;
-END $$
 
-
---Saber si un cliente existe por DNI--
-
-DELIMITER $$
-CREATE FUNCTION sf_ExisteCliente(p_dni INT)
-RETURNS BOOLEAN
-DETERMINISTIC
-BEGIN
-   RETURN EXISTS (
-       SELECT 1 FROM Cliente WHERE dni = p_dni
-   );
-END $$
